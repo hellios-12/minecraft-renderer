0d560d238f33802bc34a3296a44ae205821d57fc
hellios-12 <dangdinhbaohoang@gmail.com>
2026-07-22 22:28:26 +0700

    Fix syntax error in renderSlot.ts

--- src/three/renderSlot.ts
+++ src/three/renderSlot.ts
@@ -3,7 +3,7 @@ import { BlockModel } from 'mc-assets'
 import { versionToNumber } from 'mc-assets/dist/utils'
 import type { ResourcesManagerCommon } from '../resourcesManager'
 declare global {
-  var appViewer: any;
+  var appViewer: any
 }
 export type ResolvedItemModelRender = {
   modelName: string
